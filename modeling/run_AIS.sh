e4c85ceb1edb7bc96c83fc3db31027cba12014a2
Robert Hawkins <hawkrobe@gmail.com>
2020-02-22 01:17:16 -0500

    add more AIS output

--- modeling/run_AIS.sh
+++ modeling/run_AIS.sh
@@ -1,3 +1,3 @@
 #!/bin/bash#
-# parallel --bar --colsep ',' "sh ./run_AIS.sh {1}" :::: input/BF_grid.csv > "output/AIS.txt"
+# parallel --bar --colsep ',' "sh ./run_AIS.sh {1}" :::: input/BF_grid.csv > "output/AIS2.txt"
 ~/.webppl/webppl BDA.wppl --require ./refModule/ --require webppl-csv -- --model $1 --AIS true
